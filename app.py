e03b818d69470ebb3f5ceb834f6e83842a04efdc
ehab <ehababo1994@gmail.com>
2022-11-24 22:35:32 +0200

    improved flask

--- app.py
+++ app.py
@@ -9,8 +9,14 @@ def load_insert_item_html():
     if request.method == 'POST':
         movie_name = request.form['name']
         #print(movie_name)
-        x = mongodb()
-        x.insert_data(movie_name)
+        mongo = mongodb()
+        if request.form['submit_button'] == 'submit':
+            mongo.insert_data(movie_name)
+        elif request.form['submit_button'] == 'delete':
+            mongo.del_data(movie_name)
+        elif request.form['submit_button'] == 'read data':
+            mongo.read_data()
     return render_template('form.html')
 
 app.run()
+
